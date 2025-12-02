fefb7d2b851518892fd63930ceb013b484ed1ed0
Daniël de Vries <daniel.devries@beequip.nl>
2025-12-02 14:25:41 +0100

    Re-introduced ActiveModel for casting types
    Because removing ActiveModel requires a lot more work than we initially
    thought, we restored the original functionality.
    
    When/if we remove ActiveRecord, it would probably be a breaking change

--- lib/json_path_attribute/type.rb
+++ lib/json_path_attribute/type.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_model"
+
 module JsonPathAttribute
   # Provides a way to cast values to a specific type
   # This class is intended to be used internally by JsonPathAttribute
@@ -7,31 +9,36 @@ module JsonPathAttribute
     class << self
       def cast_attribute(type, value, array: false)
         return value if type == :source
-
-        if value.nil?
-          return [] if array
-          return false if type == :boolean
-        end
-
-        array ? cast_array(type, value) : cast_value(type, value, array)
-      end
-
-      def cast_value(type, value, array)
         return cast_object_attribute(type, value, array: array) if type.is_a?(Class)
-        return false if type == :boolean && value.nil?
 
-        case type
-        when :string, :decimal
-          value.to_s
-        when :integer
-          value.to_i
+        cast_to(type, value, array: array)
+      end
+
+      def cast_to(type, value, array: false)
+        return [] if value.nil? && array
+        return false if value.nil? && type == :boolean
+
+        cast_type = ActiveModel::Type.lookup(type)
+
+        if array
+          cast_array(value, type, cast_type)
         else
-          raise TypeError, "Unable to cast #{value.inspect} to #{type}"
+          cast_value(value, type, cast_type)
         end
       end
 
-      def cast_array(type, values)
-        values.map { |element| cast_value(type, element, false) }
+      def cast_array(values, type, cast_type)
+        values.map do |value|
+          next false if value.nil? && type == :boolean
+
+          cast_type.cast(value)
+        end
+      end
+
+      def cast_value(value, type, cast_type)
+        value = value.to_s if type == :decimal && value.is_a?(Float)
+
+        cast_type.cast(value)
       end
 
       def cast_object_attribute(type, value, array:)
